47f15fd103c5aa849a0e4dd0d9b725d24d2f975f
Tim Tse <timothy.tse@quadpay.com>
2021-06-14 17:20:59 -0400

    created card class

---
+++ src/Cards/Card.cs
@@ -0,0 +1,15 @@
+﻿namespace PokerPlayer.Cards
+{
+    class Card
+    {
+        public string Value { get; set; }
+
+        public string Suit { get; set; }
+
+        public Card(string value, string suit)
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
+    }
+}
