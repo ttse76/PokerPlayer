e9ec473bc430284a0542c453c69a6b6b5e937bc5
Tim Tse <timtse76@gmail.com>
2021-06-14 23:24:44 -0400

    created deal class

---
+++ src/Games/Deal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PokerPlayer.Games
+{
+    public class Deal
+    {
+        public int NumCards { get; set; }
+
+        public int StartBet { get; set; }
+
+        public Deal(int numCards, int startBet)
+        {
+            this.NumCards = numCards;
+            this.StartBet = startBet;
+        }
+
+    }
+}
