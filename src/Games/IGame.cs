af861d1dd432051f4c3d0da3ab424236b2065122
Tim Tse <timtse76@gmail.com>
2021-06-14 23:10:56 -0400

    created game interface

---
+++ src/Games/IGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace PokerPlayer.Games
+{
+    public interface IGame
+    {
+        // Number of cards each player should have in their hand
+        int NumCardsInHand { get; set; }
+
+        // List for how many times a dealer deals and how many cards each time
+        // Example: Texas Hold'em: [3, 1, 1]
+        List<int> DealIncrements { get; set; }
+    }
+}
