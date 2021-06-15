99b79fd0bab9e13ba64ebc4f8e3699289cf955d6
Tim Tse <timothy.tse@quadpay.com>
2021-06-15 17:39:08 -0400

    created hand analyzer

---
+++ src/Services/HandAnalyzer.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using Game2.Engine;
+using PokerPlayer.Players;
+
+namespace PokerPlayer.Services
+{
+    public static class HandAnalyzer
+    {
+        public class HandType
+        {
+            public string HandName { get; set; }
+
+            public List<Card> HandCards { get; set; }
+
+            public int HandRank { get; set; }
+
+            /*public HandType(string handName, List<Card> handCards, int handRank)
+            {
+                this.HandName = handName;
+                this.HandCards = handCards;
+                this.HandRank = handRank;
+            }*/
+        }
+        public static void CompareHands(Player p1, Player p2, List<Card> communityCards)
+        {
+        }
+
+        public static HandType GetHandType(List<Card> playerHand, List<Card> communityCards)
+        {
+            if(HandTypeChecker.IsRoyalFlush(playerHand, communityCards))
+            {
+                return new HandType()
+                {
+                    HandName = "Royal Flush",
+                    HandCards = playerHand,
+                    HandRank = 1
+                };
+            }
+
+            if(HandTypeChecker.IsFlush(playerHand, communityCards))
+            {
+                return new HandType()
+                {
+                    HandName = "Flush",
+                    HandCards = playerHand,
+                    HandRank = 1
+                };
+            }
+
+            if (HandTypeChecker.IsStraight(playerHand, communityCards))
+            {
+                return new HandType()
+                {
+                    HandName = "Straight",
+                    HandCards = playerHand,
+                    HandRank = 1
+                };
+            }
+
+            if (HandTypeChecker.IsPair(playerHand, communityCards))
+            {
+                return new HandType()
+                {
+                    HandName = "Pair",
+                    HandCards = playerHand,
+                    HandRank = 2
+                };
+            }
+
+            return new HandType()
+            {
+                HandName = "Nothing",
+                HandCards = playerHand,
+                HandRank = 999999999
+            };
+        }
+    }
+}
---
+++ src/Services/HandTypeChecker.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Game2.Engine;
+
+namespace PokerPlayer.Services
+{
+    public static class HandTypeChecker
+    {
+        private static List<Card> CombineHands(List<Card> playerCards, List<Card> communityCards)
+        {
+            var allCards = playerCards.Concat(communityCards).ToList();
+            allCards.Sort(delegate (Card a, Card b)
+            {
+                return a.CompareTo(b) * -1;
+            });
+
+            return allCards;
+        }
+
+        public static bool IsRoyalFlush(List<Card> playerCards, List<Card> communityCards)
+        {
+            var allCards = CombineHands(playerCards, communityCards);
+
+            int tenAt = 0;
+            bool has10 = false;
+
+            for(int i = 0; i < allCards.Count; i++)
+            {
+                if(allCards[i].Rank.Value == 10)
+                {
+                    if(allCards.Count - i < 5)
+                    {
+                        return false;
+                    }
+                    tenAt = i;
+                    has10 = true;
+                    break;
+                }
+            }
+
+            if(!has10)
+            {
+                return false;
+            }
+
+            if(allCards[tenAt + 1].Rank == Rank.Jack && allCards[tenAt + 1].Suit == allCards[tenAt].Suit)
+            {
+                if (allCards[tenAt + 2].Rank == Rank.Queen && allCards[tenAt + 2].Suit == allCards[tenAt].Suit)
+                {
+                    if (allCards[tenAt + 3].Rank == Rank.King && allCards[tenAt + 3].Suit == allCards[tenAt].Suit)
+                    {
+                        if (allCards[tenAt + 4].Rank == Rank.Ace && allCards[tenAt + 4].Suit == allCards[tenAt].Suit)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool IsFlush(List<Card> playerCards, List<Card> communityCards)
+        {
+            var allCards = CombineHands(playerCards, communityCards);
+
+            for(int i = 0; i < allCards.Count; i++)
+            {
+                int numSameSuit = 1;
+                var currentSuit = allCards[i].Suit;
+                for(int j = 0; j < allCards.Count; j++)
+                {
+                    if(i != j && allCards[j].Suit == currentSuit)
+                    {
+                        numSameSuit++;
+                    }
+                }
+                if(numSameSuit == 5)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool IsStraight(List<Card> playerCards, List<Card> communityCards)
+        {
+            var allCards = CombineHands(playerCards, communityCards);
+            int consecutive = 1;
+
+            for(int i = 1; i < allCards.Count; i++)
+            {
+                if(consecutive == 5)
+                {
+                    return true;
+                }
+                if(allCards[i-1].Rank.Value == allCards[i].Rank.Value - 1)
+                {
+                    consecutive++;
+                }
+                else
+                {
+                    consecutive = 1;
+                }
+            }
+            if (consecutive == 5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsPair(List<Card> playerCards, List<Card> communityCards)
+        {
+            var allCards = CombineHands(playerCards, communityCards);
+
+            for(int i = 0; i < allCards.Count; i++)
+            {
+                for(int j = i + 1; j < allCards.Count; j++)
+                {
+                    if(allCards[i].Rank.Value == allCards[j].Rank.Value)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
