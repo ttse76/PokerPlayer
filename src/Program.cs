dcced712a2604ab81f14ce1a9c761820badc8d68
Tim Tse <timothy.tse@quadpay.com>
2021-06-15 12:02:58 -0400

    basic implementation in main

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace PokerPlayer
+﻿using PokerPlayer.Games.GameTypes;
+using PokerPlayer.Table;
+
+namespace PokerPlayer
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            var tableManager = new TableManager(4, 500);
+            tableManager.Game = new TexasHoldEm(tableManager.Players);
+            tableManager.Play();
         }
     }
 }
