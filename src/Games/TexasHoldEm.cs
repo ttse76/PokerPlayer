39bf42c1955973103b6d140d26a4fd0f3bd76027
Tim Tse <timtse76@gmail.com>
2021-06-14 23:13:42 -0400

    created texas hold em implementation for game

---
+++ src/Games/TexasHoldEm.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace PokerPlayer.Games
+{
+    public class TexasHoldEm : IGame
+    {
+        public int NumCardsInHand { get; set; }
+
+        public List<int> DealIncrements { get; set; }
+
+        public TexasHoldEm()
+        {
+            this.NumCardsInHand = 2;
+            this.DealIncrements = new List<int>
+            {
+                3,
+                1,
+                1
+            };
+        }
+    }
+}
