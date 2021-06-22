7a6cd7faff34920e5a5bb773d965763461686287
Tim Tse <timothy.tse@quadpay.com>
2021-06-22 17:31:30 -0400

    added to brain class

--- src/Players/Brain.cs
+++ src/Players/Brain.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PokerPlayer.RiskConfigs;
 
 namespace PokerPlayer.Players
 {
     public class Brain
     {
+        public Guid PlayerId;
+
+        private readonly IPlayerRiskConfig RiskConfig;
+
+        public Brain(Guid playerId, IPlayerRiskConfig riskConfig)
+        {
+            this.PlayerId = playerId;
+            this.RiskConfig = riskConfig;
+        }
+
+        /*
+         * Determine if the player should bet, check, or fold
+         * 
+         */
+        public string GetAction()
+        {
+            return "check";
+        }
     }
 }
