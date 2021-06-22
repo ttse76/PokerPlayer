7344857d55d6e64dce57ffc6eeca15d8de989792
Tim Tse <timothy.tse@quadpay.com>
2021-06-22 17:44:36 -0400

    created easy config

---
+++ src/RiskConfigs/Configs/EasyConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PokerPlayer.RiskConfigs.Configs
+{
+    public class EasyConfig : IPlayerRiskConfig
+    {
+        public Guid PlayerId { get; set; }
+
+        public string ConfigName { get; set; }
+
+        public EasyConfig(Guid playerId)
+        {
+            this.PlayerId = playerId;
+            this.ConfigName = "Easy";
+        }
+    }
+}
